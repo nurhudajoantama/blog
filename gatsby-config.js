c742552da2cb97f54030c2bf763d6ed0c6584497
Nurhudaaa <nurhuda.joantama14@gmail.com>
2021-08-28 19:09:53 +0700

    adding plugin-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,28 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Blog",
   },
-  plugins: ['gatsby-plugin-postcss'],
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "styles",
+        path: `${__dirname}/src/stles/`,
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 };
